beecef3e9fee9641dd7a292e90a5d712659d2795
Dongmin Lee <medi8600@gmail.com>
2016-03-10 10:56:19 -0600

    acceaptance test passed! :D

---
+++ notes/web.rb
@@ -0,0 +1,35 @@
+require 'socket'
+class Notes
+  class Web
+    attr_accessor :server
+    def initialize(app, hash)
+      @app = app
+      @server = TCPServer.new hash[:Host], hash[:Port]
+    end
+
+    def stop
+      @server.close
+    end
+
+    def start
+      loop do
+        socket = server.accept
+        inline = socket.gets
+
+        (response_code,headers,body) = @app.call(inline)
+
+        header =  "HTTP/1.1 "+response_code.to_s+" OK\r\n" +
+          headers.map{ |k,v| "#{k}: #{v}"}.join("\r\n")
+
+
+        socket.print header
+
+        socket.print "\r\n\r\n"
+
+        socket.print body.join
+
+        socket.close
+      end
+    end
+  end
+end
