84d31e8c19a907384f0bb1e83feddbb5d722dfe3
Vince Lundgren <vincent.lundgren@gmail.com>
2016-03-14 10:22:59 -0500

    modified notes added query_string

--- notes/web.rb
+++ notes/web.rb
@@ -6,9 +6,10 @@ class Notes
   class Web
     attr_accessor :server
 
-    def initialize(app, hash)
+    def initialize(app = nil, hash)
       @server = TCPServer.new hash[:Host], hash[:Port]
-      @app =  app
+
+          @app =  app
     end
 
     def stop
@@ -51,20 +52,72 @@ class Notes
       check = {"REQUEST_METHOD" => first_line[0],
       "PATH_INFO" => first_line[1],
       "SERVER_PROTOCOL"=> first_line[2]}
+      query = []
+      if first_line[1].include?("?")
+        query  = check["PATH_INFO"][/=.*/][1..-1].split("+")
+        check["QUERY_STRING"] = query
+      else
+        check["QUERY_STRING"] = ""
+      end
+
+
 
      # require "pry"
      # binding.pry
-#   .map{ |k,v| "#{k}: #{v}"}.join("\r\n")
+     # .map{ |k,v| "#{k}: #{v}"}.join("\r\n")
       check
       end
 
 
 
-    end
-  end
+      def own_app
+          loop do
+          socket = @server.accept
+          final = []
+          final << socket.gets
+          while final[-1] != "\r\n"
+            final << socket.gets
+          end
+          check = parser(final)
+          socket.print "HTTP/1.1 200 OK\r\n"
+          socket.print "Content-Type: text/html\r\n"
+          socket.print "Content-Length: 125\r\n"
+          socket.print "\r\n"
+          socket.puts "<form action='/search' method='GET'>"
+          socket.puts "<input type='text' name='query'></input>"
+          socket.puts "<input type='Submit'></input>"
+          socket.puts "</form>"
+          socket.puts "\r\n\r\n"
 
+          require "pry"
+          binding.pry
+          socket.close
+          end
+       #   app = Proc.new do |env_hash|
+       #     path_info = env_hash['PATH_INFO']
+       #     body      = "hello, class ^_^"
+       #     [200, {'Content-Type' => 'text/plain', 'Content-Length' => body.length, 'omg' => 'bbq'}, [body]]
+       #     socket.close
+
+        #  end
+      end
+
+
+      def app_two
+
+
+
+
+
+      end
+
+  end
+  #end Web
+end
+#end Notes
 
 # server = Notes::Web.new(app, Port: port, Host: 'localhost')
 #    thread = Thread.new { server.start } # The thread allows the server to sit and wait for a request, but still return to here so we can send it.
 #    thread.abort_on_exception = true
-
+server = Notes::Web.new(Port: 6969, Host: 'localhost')
+server.own_app
